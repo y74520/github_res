060e17d69f0e7724a0a96e5252dc80fe98d99370
yhb <2090778792@qq.com>
2022-07-13 11:34:16 +0800

    第二次提交

--- src/main/java/com/testgit/TestGit.java
+++ src/main/java/com/testgit/TestGit.java
@@ -3,5 +3,6 @@ package com.testgit;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git!!!");
+        System.out.println("你好！！！");
     }
 }
